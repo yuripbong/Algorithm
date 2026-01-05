8d4dbc517faec2d58ac9da364acffc5617a8b433
yuripbong <128915302+yuripbong@users.noreply.github.com>
2026-01-05 16:26:11 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.02 ms, Memory: 9.09 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
@@ -0,0 +1,8 @@
+def solution(array, height):
+    answer = 0
+    array.sort()
+    print(array)
+    for i in range(len(array)):
+        if array[i] > height:
+            answer += 1
+    return answer
