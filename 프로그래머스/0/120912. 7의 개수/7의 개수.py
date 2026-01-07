089cc502985c6a2e0da179e57a439847cedc7d77
yuripbong <128915302+yuripbong@users.noreply.github.com>
2026-01-07 10:13:56 +0900

    [level 0] Title: 7의 개수, Time: 0.02 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.py
@@ -0,0 +1,8 @@
+def solution(array):
+    answer = 0
+    
+    for i in range(len(array)):
+        for j in range(len(str(array[i]))):
+            if str(array[i])[j] == '7':
+                answer += 1
+    return answer
