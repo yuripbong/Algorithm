29f456d30078fb18a06d1f9ee636041b395ac906
yuripbong <128915302+yuripbong@users.noreply.github.com>
2026-02-20 17:23:01 +0900

    [level 1] Title: 모의고사, Time: 6.91 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,61 +1,26 @@
 def solution(answers):
-    s1 = [1, 2, 3, 4, 5]
-    s2 = [2, 1, 2, 3, 2, 4, 2, 5]
-    s3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    ss1 = 0
-    ss2 = 0
-    ss3 = 0
-    r = []
-    i1 = 0
-    i2 = 0
-    i3 = 0
+    answer = []
+    
+    # 수포자 문제풀이 패턴
+    one = [1, 2, 3, 4, 5]
+    two = [2, 1, 2, 3, 2, 4, 2, 5]
+    three = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    score = [0, 0, 0]
+    
     
     for i in range(len(answers)):
-        if i1 == 5:
-            i1 = 0
-        if i2 == 8:
-            i2 = 0
-        if i3 == 10:
-            i3 = 0
-        if answers[i] == s1[i1]:
-            ss1 += 1
-        if answers[i] == s2[i2]:
-            ss2 += 1
-        if answers[i] == s3[i3]:
-            ss3 += 1
-        i1 += 1
-        i2 += 1
-        i3 += 1
-    
-    if len(answers) == ss1:
-        print('수포자 1은 모든 문제를 맞혔습니다.')
-    elif ss1 == 0:
-        print('수포자 1은 모든 문제를 틀렸습니다.')
-    if len(answers) == ss2:
-        print('수포자 2는 모든 문제를 맞혔습니다.')
-    elif ss2 == 0:
-        print('수포자 2는 모든 문제를 틀렸습니다.')
-    if len(answers) == ss3:
-        print('수포자 3은 모든 문제를 맞혔습니다.')
-    elif ss3 == 0:
-        print('수포자 3은 모든 문제를 틀렸습니다.')
-        
-    if ss1 == ss2 and ss1 == ss3:
-        print(f'모든 사람이 {ss1}문제씩을 맞췄습니다.')
-        return [1, 2, 3]
-    
-    result = sorted([ss1, ss2, ss3], reverse=True)
-    
-    # 점수와 번호를 함께 묶고 정렬
-    score_list = [(1, ss1), (2, ss2), (3, ss3)]
-    score_list.sort(key=lambda x: x[1], reverse=True)
-
-    # 점수가 같은 사람도 포함하되, 중복 없이 순서대로
-    max_score = score_list[0][1]
-    for person, score in score_list:
-        if score == max_score:
-            r.append(person)
-        elif score < max_score:
-            break
+        if answers[i] == one[i % len(one)]:
+            score[0] += 1
             
-    return r
+        if answers[i] == two[i % len(two)]:
+            score[1] += 1
+            
+        if answers[i] == three[i % len(three)]:
+            score[2] += 1
+            
+    max_score = max(score)
+    for i in range(len(score)):
+        if score[i] == max_score:
+            answer.append(i+1)
+    
+    return answer
