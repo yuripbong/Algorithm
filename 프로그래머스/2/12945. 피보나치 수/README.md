72b25c5b55ad83a0593ecd8b964367ee21bd4fcf
yuripbong <128915302+yuripbong@users.noreply.github.com>
2026-02-24 17:59:43 +0900

    [level 2] Title: 피보나치 수, Time: 519.28 ms, Memory: 456 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/README.md
+++ 프로그래머스/2/12945. 피보나치 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피보나치 수 - 12945 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12945?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12945) 
 
 ### 성능 요약
 
-메모리: 9.16 MB, 시간: 140.67 ms
+메모리: 456 MB, 시간: 519.28 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 28일 21:58:07
+2026년 02월 24일 17:59:40
 
 ### 문제 설명
 
--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,10 +1,11 @@
-def solution(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    a, b = 0, 1
-    for _ in range(n):
-        a, b = b, a+b
-    answer = a % 1234567
-    return answer
+def solution(x):
+    # f = [0, 1, 1, 2, 3, 5]
+    f = [0] * (x+1)
+    f[0] = 0
+    f[1] = 1
+    
+    for i in range(2, x+1):
+        f[i] = f[i-2] + f[i-1]
+        
+    
+    return f[-1] % 1234567
